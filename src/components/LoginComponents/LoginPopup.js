427627779748bc1355c308faf137fb70fd1a533f
Alim1899 <alimakhchanogli@gmail.com>
2023-05-21 16:40:40 +0400

    Completed LoginSetup design

--- src/components/LoginComponents/LoginPopup.js
+++ src/components/LoginComponents/LoginPopup.js
@@ -1,13 +1,16 @@
-import React from 'react'
-import classes from './LoginPopup.module.css';
+import React from "react";
+import classes from "./LoginPopup.module.css";
 
 const LoginPopup = () => {
   return (
     <div className={classes.section}>
-    <div><button>Sign Up</button><p>or, Already have an Acoount?</p> <button>Login</button></div>
-        
+      <div>
+        <button>Sign Up</button>
+        <p className={classes.quest}>New customer?</p>
+        <a href=" " onClick={(e)=>{e.preventDefault()}}>Login</a>{" "}
+      </div>
     </div>
-  )
-}
+  );
+};
 
 export default LoginPopup;
