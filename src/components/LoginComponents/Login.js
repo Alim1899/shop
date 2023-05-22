7926a62d92a860ff9eca8ef81d8eb477f1561410
Alim1899 <alimakhchanogli@gmail.com>
2023-05-22 16:35:33 +0400

    Retrieved countries(flags, dial codes) and designed

--- src/components/LoginComponents/Login.js
+++ src/components/LoginComponents/Login.js
@@ -1,44 +1,75 @@
 import React, { useRef, useEffect, useState, useCallback } from "react";
 import classes from "./Login.module.css";
-import Navbar from "../../UI/Layout/Navbar";
+import codes from './CountryCodes.json';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCaretDown } from "@fortawesome/free-solid-svg-icons";
 const Login = () => {
   const nameInput = useRef();
   const lastnameInput = useRef();
   const emailInput = useRef();
   const numberInput = useRef();
   const [dataArrived, setDataArrived] = useState(false);
-  const [names] = useState([]);
+ 
   const [open, setOpen] = useState(false);
+  const [country, setCountry] = useState();
+  const [list, showList] = useState(false);
+  const [mergedList, setMergedList] = useState([]);
+
+
+
+
+
+ 
+
+  const selectCountry = (e) => {
+    e.preventDefault();
+
+     if (!country) setCountry(e.currentTarget.childNodes[0].childNodes[0].src);
+    setOpen(false);
+    showList(false);
+  };
+
   const handleMenu = (e) => {
-    e.preventDefault()
+    e.preventDefault();
     setOpen(!open);
+    showList(!list);
   };
 
   const fetchData = useCallback(async () => {
     await fetch("https://restcountries.com/v3.1/all?fields=name,flags")
       .then((response) => response.json())
       .then((data) => {
-        console.log(data);
-        data.map((el) => {
-          return names.push({
-            name: el.name.common,
-            src: el.flags.svg,
-            alt: el.flags.alt,
-          });
-        });
+        const full = [];
+       console.log(mergedList);
+        codes.forEach((el) => {
+          
+          data.forEach((code)=>{
+            
+            if(el.name===code.name.common){
+              full.push({
+                name:code.name.common,
+                src:code.flags.svg,
+                alt:code.flags.alt,
+                dial:el.dial_code,
+                code:el.code
+              })
+
+            }
+
+          })
+       });
+        setMergedList(full);
+        console.log(mergedList);
         setDataArrived(true);
-        names.sort();
-        console.log(data[0].flags);
-        console.log(names);
+       
       });
-  }, [names]);
+  }, []);
 
   useEffect(() => {
     fetchData();
   }, [fetchData]);
   return (
     <div className={classes.main}>
-      <Navbar />
       <div className={classes.content}>
         <form className={classes.form}>
           <div className={classes.fullname}>
@@ -50,32 +81,44 @@ const Login = () => {
           <div className={classes.contact}>
             <label htmlFor="email">Email</label>
             <input id="email" type="email" ref={emailInput}></input>
-            <label htmlFor="number">Choose your country</label>
 
             <div className={classes.drop}>
-              <button className={classes.dropBtn} onClick={handleMenu}>Handle</button>
-              
-               
-                  <ul className={classes.menu}>
+              <button className={classes.dropBtn} onClick={handleMenu}>
+                Choose your country <FontAwesomeIcon icon={faCaretDown} />
+              </button>
 
-                   {dataArrived && open &&names.map((el) => (
-                    <li key={Math.random()} className={classes.item}>
-                      <button>
-                        <img
-                          className={classes.flag}
-                          src={el.src}
-                          alt={el.alt}
-                        ></img>
-                        <h6>{el.name}</h6>
-                      </button>
-                    </li>
-                 
-                ))}
+              {list && (
+                <ul className={classes.menu}>
+                  {dataArrived &&
+                    open &&
+                    mergedList.map((el) => (
+                      <li
+                        onClick={selectCountry}
+                        key={Math.random()}
+                        className={classes.item}
+                      >
+                        <button disabled>
+                          <img
+                            className={classes.flag}
+                            src={el.src}
+                            alt={el.alt}
+                          ></img>
+                        </button>
+                        <h6 className={classes.name}>{el.name}</h6>
+                        <h6 className={classes.code}>{el.dial}</h6>
+                      </li>
+                    ))}
                 </ul>
-                
+              )}
             </div>
-
-            <input id="number" type="number" ref={numberInput}></input>
+            {country && (
+              <div className={classes.number}>
+                
+                <label htmlFor="number">Mobile Number:</label>
+                <img src={country.src} alt={country.alt}></img>
+                <input id="number" type="number" ref={numberInput}></input>
+              </div>
+            )}
           </div>
         </form>
       </div>
