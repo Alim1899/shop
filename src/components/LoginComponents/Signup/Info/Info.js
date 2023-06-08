2dad1b36c891545afebf67eaff32e8c18d85a776
Alim1899 <alimakhchanogli@gmail.com>
2023-06-08 23:31:15 +0400

    Added age and gender fields

--- src/components/LoginComponents/Signup/Info/Info.js
+++ src/components/LoginComponents/Signup/Info/Info.js
@@ -1,8 +1,17 @@
 import React, { useRef } from "react";
+import classes from "./Info.module.css";
 const Info = () => {
   const nameInput = useRef();
   const lastnameInput = useRef();
   const emailInput = useRef();
+  const dayInput = useRef();
+  const monthInput = useRef();
+  const yearInput = useRef();
+  const age = (e) => {
+    const today = new Date();
+    console.log(today.getMonth());
+  };
+
   return (
     <div>
       <label htmlFor="name">Name</label>
@@ -21,6 +30,24 @@ const Info = () => {
         placeholder="Mc Gregory"
         ref={lastnameInput}
       ></input>
+      <label htmlFor="age">Birth date
+        <div onChange={age} className={classes.age}>
+          
+          <input placeholder="dd" min='1' max='31' type="number" ref={dayInput}></input>
+          <input placeholder="mm" min='1' max='12' type="number" ref={monthInput}></input>
+          <input placeholder="yyyy" min='1900' max='2022' type="number"  ref={yearInput}></input>
+        </div>{" "}
+      </label>
+      
+      <select className={classes.select}>
+      <optgroup label="Gender">
+      <option disabled>Gender</option>
+         <option >Male</option>
+        <option>Female</option>
+      </optgroup>
+       
+      </select>
+
       <label htmlFor="email">Email</label>
       <input
         id="email"
