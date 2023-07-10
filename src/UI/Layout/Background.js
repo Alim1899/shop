91b7853c873213162b58c3b8e6e83d64359975dd
Alim1899 <alimakhchanogli@gmail.com>
2023-07-11 00:13:33 +0400

    Added error page

--- src/UI/Layout/Background.js
+++ src/UI/Layout/Background.js
@@ -1,46 +1,55 @@
-import React,{useState} from "react";
+import React, { useState } from "react";
 import background from "../../assets/hike.jpeg";
 import Home from "./Home";
 import SignHandler from "../../components/LoginComponents/Data/SignHandler";
 import LoginHandler from "../../components/LoginComponents/Data/LoginHandler";
 import classes from "./Background.module.css";
+import ErrorPage from "./ErrorPage";
 
 const Background = () => {
   const [homePage, setHomePage] = useState(false);
   const [signupPage, setSignupPage] = useState(false);
   const [loginPage, setLoginPage] = useState(false);
+  const [errorPage, setErrorPage] = useState(false);
   const pageHandler = () => {
     if (window.location.pathname === "/") {
       setHomePage(true);
       setSignupPage(false);
       setLoginPage(false);
+      setErrorPage(false);
+      console.log(window.location.pathname);
       return;
-    }
-    if (window.location.pathname === "/signup") {
-      setHomePage(false);
+    } else if (window.location.pathname === "/signup") {
       setSignupPage(true);
-      setLoginPage(false);
-      return;
-    }
+      setHomePage(false);
 
-    if (window.location.pathname === "/login") {
+      setLoginPage(false);
+      setErrorPage(false);
+
+      console.log(window.location.pathname);
+      return;
+    } else if (window.location.pathname === "/login") {
+      setLoginPage(true);
       setHomePage(false);
       setSignupPage(false);
-      setLoginPage(true);
+
+      console.log(window.location.pathname);
       return;
+    } else {
+      setErrorPage(true);
     }
   };
   return (
     <div onLoad={pageHandler} className={classes.background}>
       <div className={classes.back}>
         <img alt="background" src={background}></img>
-        <div >
-      {homePage && <Home />}
-      {signupPage && <SignHandler />}
-      {loginPage && <LoginHandler />}
+        <div>
+          {homePage && <Home />}
+          {signupPage && <SignHandler />}
+          {loginPage && <LoginHandler />}
+          {errorPage&&<ErrorPage/> }
+        </div>
       </div>
-      </div>
-      
     </div>
   );
 };
---
+++ src/UI/Layout/ErrorPage.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const ErrorPage = () => {
+  return (
+    <div>
+      <h1>Oooopsss.....</h1>
+      <p>You entered and invalid path</p>
+    </div>
+  );
+};
+
+export default ErrorPage;
