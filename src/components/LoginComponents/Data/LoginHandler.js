b315e33fd85eef671e2ecdeb8276edcbf1416e5a
Alim1899 <alimakhchanogli@gmail.com>
2023-08-09 03:34:38 +0400

    Handled entered user details correct or not

--- src/components/LoginComponents/Data/LoginHandler.js
+++ src/components/LoginComponents/Data/LoginHandler.js
@@ -1,19 +1,23 @@
 import Login from "../Login/Login";
 import classes from "./LoginHandler.module.css";
 import { useState } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faRightToBracket } from "@fortawesome/free-solid-svg-icons";
 const LoginHandler = (props) => {
   const [rightUserDetails, setRightUserDetails] = useState(false);
   //Fetching data from firebase
+  const enteredUser = [];
+  const registeredUsers = [];
   const fetchData = async () => {
     fetch("https://hikemart-2877b-default-rtdb.firebaseio.com/users.json")
       .then((response) => response.json())
       .then((data) => {
-        console.log(Object.entries(data));
+        Object.entries(data).map((el) => registeredUsers.push(el[1]));
       });
   };
-  // fetchData();
+  fetchData();
 
-  const enteredDetails = async (values) => {
+  const handleEnteredDetails = async (values) => {
     await fetch(
       "https://hikemart-2877b-default-rtdb.firebaseio.com/enters.json",
       {
@@ -24,13 +28,16 @@ const LoginHandler = (props) => {
         },
       }
     );
-    const enteredUser = [];
+
     await fetch(
       "https://hikemart-2877b-default-rtdb.firebaseio.com/enters.json"
     )
       .then((response) => response.json())
       .then((data) => {
-        Object.entries(data).map((el) => enteredUser.push(Object.entries(el)[1][1]));
+        if (data) enteredUser.pop();
+        Object.entries(data).map((el) =>
+          enteredUser.push(Object.entries(el)[1][1])
+        );
       });
     await fetch(
       "https://hikemart-2877b-default-rtdb.firebaseio.com/enters.json",
@@ -41,12 +48,37 @@ const LoginHandler = (props) => {
         },
       }
     );
-    console.log(enteredUser);
+    if (enteredUser.length > 0) {
+      registeredUsers.every((el) => {
+        if (
+          enteredUser[0].email === el.email &&
+          enteredUser[0].password === el.password
+        ) {
+          console.log("User matches");
+          setRightUserDetails(true);
+          return false;
+        } else {
+          console.log("User not match");
+          return false;
+        }
+      });
+    }
   };
 
   return (
     <div className={classes.loginPage}>
-      <Login login={enteredDetails} />
+      <Login login={handleEnteredDetails} />
+      {rightUserDetails && (
+        <div className={classes.succes}>
+          <h2>You entered right user details</h2>
+          <h4>Go to your account</h4>
+          <a href="login" className={classes.close}>
+            {" "}
+            <FontAwesomeIcon icon={faRightToBracket} />
+            <h6>Browse</h6>
+          </a>
+        </div>
+      )}
     </div>
   );
 };
